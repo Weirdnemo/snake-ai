152e3d5334b52163580eed8fc296ff47f66a8136
Nimesh Chauhan <nimesh.chn@gmail.com>
2024-11-13 13:42:04 +0530

    changed the prediction type from tensorflow to torch, also created a trainer module in pytorch

--- agent.py
+++ agent.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
 
 MAX_MEMORY = 100000
 BATCH_SIZE = 1000
@@ -13,23 +14,13 @@ class Agent:
     def __init__(self):
         self.n_games = 0
         self.epsilon = 0 #control the randomness
-        self.gamma = 0
+        self.gamma = 0.9
         self.memory = deque(maxlen=MAX_MEMORY) #popleft()
+        self.model = Linear_QNet(11, 256, 3)
+        self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
         #model #trainer
 
     def get_state(self, game):
-        pass
-
-    def remember(self, state, action, reward, next_state, done):
-        pass
-
-    def train_long_memory(self  ):
-        pass
-
-    def train_short_memory(self, state, action, reward, next_state, done ):
-        pass
-
-    def get_action(self, state):
         head = game.snake[0]
         point_l = Point(head.x - 20, head.y)
         point_r = Point(head.x + 20, head.y)
@@ -71,9 +62,40 @@ class Agent:
             game.food.x > game.head.x,  # food right
             game.food.y < game.head.y,  # food up
             game.food.y > game.head.y  # food down
-            ]
+        ]
 
         return np.array(state, dtype=int)
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def train_long_memory(self  ):
+        if len(self.memory) > BATCH_SIZE:
+            mini_sample = random.sample(self.memory, BATCH_SIZE)
+
+        else:
+            mini_sample = self.memory
+
+        state, action, reward, next_state, done = zip(*mini_sample)
+        self.trainer.train_step(states, actions, reward, next_state, done)
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+    def get_action(self, state):
+        self.epsilon = 80 - self.n_games
+        final_move = [0, 0, 0]
+        if random.randint(0, 200) < self.epsilon:
+            move = random.randint(0, 2)
+            final_move[move] = 1
+
+        else:
+            state0 = torch.tensor(state, dtype=torch.float)
+            prediction = self.model(state0)
+            move = torch.argmax(prediction).item()
+            final_move[move] = 1
+
+        return final_move
 
 def train():
     plot_scores = []
@@ -104,6 +126,7 @@ def train():
 
             if score > record:
                 record = score
+                agent.model.save()
             print('Game', agent.n_games, 'Score', score, 'Record', record)
 
 if __name__ == '__main__':
